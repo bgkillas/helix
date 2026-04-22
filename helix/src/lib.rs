cda2602d0a73fdc72db0950ef44e5dec986fc2c9
bgkillas <walhelm@proton.me>
2026-04-22 00:12:46 -0400

    maybe reasonable api

--- helix/src/lib.rs
+++ helix/src/lib.rs
@@ -1,6 +1,5 @@
 #[noita_api::lua_module(true)]
 mod lua {
-    use noita_api::alloc::{box_new, free, malloc};
     use std::sync::{LazyLock, Mutex};
     use std::time::Instant;
     static TIME: LazyLock<Mutex<Instant>> = LazyLock::new(|| Mutex::new(Instant::now()));
@@ -9,18 +8,6 @@ mod lua {
         let mut time = TIME.lock().unwrap();
         noita_api::game_print!("update {}", time.elapsed().as_micros());
         *time = Instant::now();
-        let tmr = Instant::now();
-        let ptr = malloc::<usize>();
-        std::hint::black_box(ptr);
-        noita_api::game_print!("malloc {}", tmr.elapsed().as_nanos());
-        let tmr = Instant::now();
-        let ptrbox = box_new(10);
-        let ptrbox = std::hint::black_box(ptrbox);
-        noita_api::game_print!("box {}", tmr.elapsed().as_nanos());
-        let tmr = Instant::now();
-        free(ptr);
-        ptrbox.free();
-        noita_api::game_print!("free {}", tmr.elapsed().as_nanos());
     }
     #[lua_function]
     fn post_update() {
--- noita_api/src/alloc.rs
+++ noita_api/src/alloc.rs
@@ -16,39 +16,48 @@ static MSVCR: LazyLock<Msvcr> = LazyLock::new(|| unsafe {
         operator_delete,
     }
 });
-pub fn malloc<T: Sized>() -> NonNull<T> {
-    unsafe { NonNull::new_unchecked((MSVCR.operator_new)(size_of::<T>() as c_uint).cast()) }
-}
-pub fn box_new<T: Sized>(value: T) -> NoitaBoxOwned<T> {
-    let ptr = malloc();
-    unsafe {
-        ptr.write(value);
-    }
-    NoitaBoxOwned {
-        ptr,
-        phantom_data: PhantomData,
-    }
-}
-pub fn free<T: Sized>(ptr: NonNull<T>) {
-    unsafe { (MSVCR.operator_delete)(ptr.as_ptr().cast()) }
+#[repr(transparent)]
+pub struct Ptr<T: Sized> {
+    ptr: NonNull<T>,
 }
 #[repr(transparent)]
 pub struct NoitaBoxOwned<T: Sized> {
-    ptr: NonNull<T>,
+    ptr: Ptr<T>,
     phantom_data: PhantomData<T>,
 }
 #[repr(transparent)]
 pub struct NoitaBox<T: Sized> {
-    ptr: NonNull<T>,
+    ptr: Ptr<T>,
+}
+impl<T: Sized> Ptr<T> {
+    pub fn malloc() -> Self {
+        let ptr = unsafe {
+            NonNull::new_unchecked((MSVCR.operator_new)(size_of::<T>() as c_uint).cast())
+        };
+        Self { ptr }
+    }
+    pub fn free(&mut self) {
+        unsafe { (MSVCR.operator_delete)(self.ptr.as_ptr().cast()) }
+    }
 }
 impl<T: Sized> NoitaBox<T> {
-    pub fn free(self) {
-        free(self.ptr)
+    pub fn free(&mut self) {
+        self.ptr.free()
     }
 }
 impl<T: Sized> NoitaBoxOwned<T> {
-    pub fn free(self) {
-        free(self.ptr)
+    pub fn free(&mut self) {
+        self.ptr.free()
+    }
+    pub fn new(value: T) -> Self {
+        let ptr = Ptr::malloc();
+        unsafe {
+            ptr.write(value);
+        }
+        Self {
+            ptr,
+            phantom_data: PhantomData,
+        }
     }
 }
 impl<T: Sized> Deref for NoitaBox<T> {
@@ -71,6 +80,17 @@ impl<T: Sized> Deref for NoitaBoxOwned<T> {
 impl<T: Sized> DerefMut for NoitaBoxOwned<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         unsafe { self.ptr.as_mut() }
+    }
+}
+impl<T: Sized> Deref for Ptr<T> {
+    type Target = NonNull<T>;
+    fn deref(&self) -> &Self::Target {
+        &self.ptr
+    }
+}
+impl<T: Sized> DerefMut for Ptr<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.ptr
     }
 }
 impl<T: Sized> From<NoitaBoxOwned<T>> for NoitaBox<T> {
