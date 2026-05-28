16fba04c92a43f4db0cb2945695f1b9683abe9a7
bgkillas <walhelm@proton.me>
2026-05-27 20:32:43 -0400

    add explosion lines and more benchmarks

--- better_explosions/better_explosions/src/lib.rs
+++ better_explosions/better_explosions/src/lib.rs
@@ -7,7 +7,9 @@ pub mod circumference;
 pub mod line;
 pub mod octant;
 use crate::arc::ArcIter;
+use crate::circumference::Circumference;
 use crate::line::LineIter;
+use crate::octant::octant;
 use noita_api::{Cell, CellData, ConfigExplosion, GameGlobal, GridWorld, StdBox, Vec2, this_call};
 use rand::RngExt as _;
 use rand::distr::Bernoulli;
@@ -159,9 +161,79 @@ impl ExplosionManager {
             }
         }
     }
+    #[inline]
+    pub fn explosion_lines(&self, config: &ConfigExplosion, pos: Vec2<f32>) {
+        let mut rng = rand::rng();
+        let game_global = GameGlobal::global();
+        let cell_create_id = game_global
+            .m_cell_factory
+            .material_ids
+            .get(&config.create_cell_material)
+            .copied()
+            .unwrap_or_default();
+        let cell_create = StdBox::from(&game_global.m_cell_factory.cell_data[cell_create_id]);
+        let grid_world = game_global.m_grid_world;
+        let chunk_map = grid_world.chunk_map.chunk_array;
+        let ix0 = (512 * 256 + truncate_f32(pos.x)).cast_unsigned();
+        let iy0 = (512 * 256 + truncate_f32(pos.y)).cast_unsigned();
+        let chance = if cell_create_id == 0 {
+            0
+        } else {
+            u32::try_from(config.create_cell_probability).unwrap()
+        };
+        let bern = Bernoulli::from_ratio(chance, 100).unwrap();
+        let r = truncate_f32u(config.explosion_radius);
+        for (ix1, iy1) in Circumference::new(r) {
+            octant(ix0, iy0, ix1, iy1, |_, ix2, iy2| {
+                let mut energy = config.ray_energy;
+                for (x, y) in LineIter::new(ix0, iy0, ix2, iy2) {
+                    let px = x % 512;
+                    let py = y % 512;
+                    if let Some(mut c) = chunk_map[y / 512][x / 512] {
+                        if let Some(p) = c[py][px] {
+                            if p.material.durability <= config.max_durability_to_destroy
+                                && config.hole_enabled
+                                && let Some(new) = energy.checked_sub(p.hp)
+                            {
+                                energy = new;
+                                p.ptr.free();
+                            } else {
+                                break;
+                            }
+                        }
+                        if rng.sample(bern) {
+                            c[py][px] = (self.construct_cell)(
+                                grid_world,
+                                x.cast_signed() - 512 * 256,
+                                y.cast_signed() - 512 * 256,
+                                cell_create,
+                                std::ptr::null_mut(),
+                            );
+                        } else {
+                            c[py][px] = None;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+            });
+        }
+    }
 }
 #[bench]
-fn bench0_empty_explosion(bencher: &mut test::Bencher) {
+fn bench0_setup(_: &mut test::Bencher) {
+    let mut game_global = GameGlobal::global();
+    game_global
+        .m_cell_factory
+        .generate_cell_data(include_str!("../../materials.xml"))
+        .unwrap();
+}
+#[cfg(test)]
+fn empty_explosion(
+    r: f32,
+    bencher: &mut test::Bencher,
+    f: fn(&ExplosionManager, &ConfigExplosion, Vec2<f32>),
+) {
     use noita_api::{Chunk, GameGlobal};
     use std::hint::black_box;
     let mut game_global = GameGlobal::global();
@@ -172,8 +244,11 @@ fn bench0_empty_explosion(bencher: &mut test::Bencher) {
     let grid_world = game_global.m_grid_world;
     let mut chunk_map = grid_world.chunk_map.chunk_array;
     chunk_map[256][256] = black_box(Some(StdBox::new(Chunk::default())));
+    chunk_map[255][256] = black_box(Some(StdBox::new(Chunk::default())));
+    chunk_map[256][255] = black_box(Some(StdBox::new(Chunk::default())));
+    chunk_map[255][255] = black_box(Some(StdBox::new(Chunk::default())));
     let mut config = ConfigExplosion::default();
-    config.explosion_radius = 200.0;
+    config.explosion_radius = r;
     config.max_durability_to_destroy = 12;
     config.ray_energy = usize::MAX;
     let game_global = GameGlobal::global();
@@ -181,18 +256,22 @@ fn bench0_empty_explosion(bencher: &mut test::Bencher) {
     config.create_cell_probability = 0;
     config.hole_enabled = true;
     let c = black_box(config);
-    let pos = black_box(Vec2 { x: 256.0, y: 256.0 });
+    let pos = black_box(Vec2 { x: 10.0, y: 10.0 });
     let em = ExplosionManager {
         construct_cell: dummy,
     };
-    bencher.iter(|| em.explosion(&c, pos))
+    bencher.iter(|| f(&em, &c, pos));
 }
-#[bench]
-fn bench1_half_explosion(bencher: &mut test::Bencher) {
+#[cfg(test)]
+fn half_explosion(
+    r: f32,
+    bencher: &mut test::Bencher,
+    f: fn(&ExplosionManager, &ConfigExplosion, Vec2<f32>),
+) {
     use noita_api::GameGlobal;
     use std::hint::black_box;
     let mut config = ConfigExplosion::default();
-    config.explosion_radius = 200.0;
+    config.explosion_radius = r;
     config.max_durability_to_destroy = 12;
     config.ray_energy = usize::MAX;
     let game_global = GameGlobal::global();
@@ -200,29 +279,38 @@ fn bench1_half_explosion(bencher: &mut test::Bencher) {
     config.create_cell_probability = 50;
     config.hole_enabled = true;
     let c = black_box(config);
-    let pos = black_box(Vec2 { x: 256.0, y: 256.0 });
+    let pos = black_box(Vec2 { x: 10.0, y: 10.0 });
     let em = ExplosionManager {
         construct_cell: dummy,
     };
-    bencher.iter(|| em.explosion(&c, pos))
+    bencher.iter(|| f(&em, &c, pos));
 }
-#[bench]
-fn bench2_empty_explosion_wall(bencher: &mut test::Bencher) {
+#[cfg(test)]
+fn empty_explosion_wall(
+    r: f32,
+    bencher: &mut test::Bencher,
+    f: fn(&ExplosionManager, &ConfigExplosion, Vec2<f32>),
+) {
     use noita_api::{Chunk, GameGlobal};
     use std::hint::black_box;
     let game_global = GameGlobal::global();
     let grid_world = game_global.m_grid_world;
     let mut chunk_map = grid_world.chunk_map.chunk_array;
-    let mut chunk = Chunk::default();
+    let mut chunk1 = Chunk::default();
+    let mut chunk3 = Chunk::default();
     let cell_data = StdBox::from(&game_global.m_cell_factory.cell_data[58]);
     for y in 0..512 {
-        for x in 300..512 {
-            chunk[y][x] = Some(Cell::new(cell_data))
+        for x in 100..512 {
+            chunk1[y][x] = Some(Cell::new(cell_data));
+            chunk3[y][x] = Some(Cell::new(cell_data));
         }
     }
-    chunk_map[256][256] = black_box(Some(StdBox::new(chunk)));
+    chunk_map[256][256] = black_box(Some(StdBox::new(Chunk::default())));
+    chunk_map[255][256] = black_box(Some(StdBox::new(chunk1)));
+    chunk_map[256][255] = black_box(Some(StdBox::new(Chunk::default())));
+    chunk_map[255][255] = black_box(Some(StdBox::new(chunk3)));
     let mut config = ConfigExplosion::default();
-    config.explosion_radius = 200.0;
+    config.explosion_radius = r;
     config.max_durability_to_destroy = 12;
     config.ray_energy = usize::MAX;
     let game_global = GameGlobal::global();
@@ -230,18 +318,22 @@ fn bench2_empty_explosion_wall(bencher: &mut test::Bencher) {
     config.create_cell_probability = 0;
     config.hole_enabled = true;
     let c = black_box(config);
-    let pos = black_box(Vec2 { x: 256.0, y: 256.0 });
+    let pos = black_box(Vec2 { x: 10.0, y: 10.0 });
     let em = ExplosionManager {
         construct_cell: dummy,
     };
-    bencher.iter(|| em.explosion(&c, pos))
+    bencher.iter(|| f(&em, &c, pos));
 }
-#[bench]
-fn bench3_half_explosion_wall(bencher: &mut test::Bencher) {
+#[cfg(test)]
+fn half_explosion_wall(
+    r: f32,
+    bencher: &mut test::Bencher,
+    f: fn(&ExplosionManager, &ConfigExplosion, Vec2<f32>),
+) {
     use noita_api::GameGlobal;
     use std::hint::black_box;
     let mut config = ConfigExplosion::default();
-    config.explosion_radius = 200.0;
+    config.explosion_radius = r;
     config.max_durability_to_destroy = 12;
     config.ray_energy = usize::MAX;
     let game_global = GameGlobal::global();
@@ -249,11 +341,139 @@ fn bench3_half_explosion_wall(bencher: &mut test::Bencher) {
     config.create_cell_probability = 50;
     config.hole_enabled = true;
     let c = black_box(config);
-    let pos = black_box(Vec2 { x: 256.0, y: 256.0 });
+    let pos = black_box(Vec2 { x: 10.0, y: 10.0 });
     let em = ExplosionManager {
         construct_cell: dummy,
     };
-    bencher.iter(|| em.explosion(&c, pos))
+    bencher.iter(|| f(&em, &c, pos));
+}
+#[bench]
+fn bench1_200_empty(bencher: &mut test::Bencher) {
+    empty_explosion(200.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench2_200_half(bencher: &mut test::Bencher) {
+    half_explosion(200.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench3_200_empty_wall(bencher: &mut test::Bencher) {
+    empty_explosion_wall(200.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench4_200_half_wall(bencher: &mut test::Bencher) {
+    half_explosion_wall(200.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench1_032_empty(bencher: &mut test::Bencher) {
+    empty_explosion(32.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench2_032_half(bencher: &mut test::Bencher) {
+    half_explosion(32.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench3_032_empty_wall(bencher: &mut test::Bencher) {
+    empty_explosion_wall(32.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench4_032_half_wall(bencher: &mut test::Bencher) {
+    half_explosion_wall(32.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench1_064_empty(bencher: &mut test::Bencher) {
+    empty_explosion(64.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench2_064_half(bencher: &mut test::Bencher) {
+    half_explosion(64.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench3_064_empty_wall(bencher: &mut test::Bencher) {
+    empty_explosion_wall(64.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench4_064_half_wall(bencher: &mut test::Bencher) {
+    half_explosion_wall(64.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench1_480_empty(bencher: &mut test::Bencher) {
+    empty_explosion(480.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench2_480_half(bencher: &mut test::Bencher) {
+    half_explosion(480.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench3_480_empty_wall(bencher: &mut test::Bencher) {
+    empty_explosion_wall(480.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench4_480_half_wall(bencher: &mut test::Bencher) {
+    half_explosion_wall(480.0, bencher, ExplosionManager::explosion);
+}
+#[bench]
+fn bench1_200_empty_lines(bencher: &mut test::Bencher) {
+    empty_explosion(200.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench2_200_half_lines(bencher: &mut test::Bencher) {
+    half_explosion(200.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench3_200_empty_wall_lines(bencher: &mut test::Bencher) {
+    empty_explosion_wall(200.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench4_200_half_wall_lines(bencher: &mut test::Bencher) {
+    half_explosion_wall(200.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench1_032_empty_lines(bencher: &mut test::Bencher) {
+    empty_explosion(32.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench2_032_half_lines(bencher: &mut test::Bencher) {
+    half_explosion(32.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench3_032_empty_wall_lines(bencher: &mut test::Bencher) {
+    empty_explosion_wall(32.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench4_032_half_wall_lines(bencher: &mut test::Bencher) {
+    half_explosion_wall(32.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench1_064_empty_lines(bencher: &mut test::Bencher) {
+    empty_explosion(64.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench2_064_half_lines(bencher: &mut test::Bencher) {
+    half_explosion(64.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench3_064_empty_wall_lines(bencher: &mut test::Bencher) {
+    empty_explosion_wall(64.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench4_064_half_wall_lines(bencher: &mut test::Bencher) {
+    half_explosion_wall(64.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench1_480_empty_lines(bencher: &mut test::Bencher) {
+    empty_explosion(480.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench2_480_half_lines(bencher: &mut test::Bencher) {
+    half_explosion(480.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench3_480_empty_wall_lines(bencher: &mut test::Bencher) {
+    empty_explosion_wall(480.0, bencher, ExplosionManager::explosion_lines);
+}
+#[bench]
+fn bench4_480_half_wall_lines(bencher: &mut test::Bencher) {
+    half_explosion_wall(480.0, bencher, ExplosionManager::explosion_lines);
 }
 #[allow(clippy::cast_possible_truncation)]
 #[allow(clippy::as_conversions)]
